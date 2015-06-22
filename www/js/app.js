f79a6a131e99ef3df925ce5800dfd8df46c82776
MoezGholami <ali.moez.gholami@gmail.com>
2015-06-22 23:00:28 +0430

    angular compatiblity error

---
+++ www/js/app.js
@@ -0,0 +1,52 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+
+angular.module('starter', ['ionic', 'chart.js'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+
+.controller('GraphCtrl', function($scope) { // Add a simple controller
+	$scope.graph = {};                        // Empty graph object to hold the details for this graph
+	$scope.graph.options={ showXLabels: 10 };
+	$scope.graph.data = [MicroData];                   // Add bar data, this will set your bars height in the graph
+	$scope.graph.colors=
+	[{
+		fillColor: 'rgba(32, 111, 204, 0.4)',
+		strokeColor: 'rgb(56, 117, 214)',
+	}];
+	$scope.graph.labels = XAxis;    // Add labels for the X-axis
+	$scope.graph.series = ['Awake'];  // Add information for the hover/touch effect
+	$scope.resetData=function()
+	{
+		synchData();
+	}
+
+});
+
+function init_data_values()
+{
+	BlueDevMacAdr="AA:BB:CC:DD:EE:FF";
+	MicroDataSize=20;
+	MicroData=[];
+	XAxis=[];
+	for(var i=0; i<MicroDataSize; i++)
+	{
+		XAxis[i]=i+1;
+		MicroData[i]=Math.random()*15+5;
+	}
+}
---
+++ www/js/blue.js
@@ -0,0 +1,57 @@
+var ConnectionInterrupted=true;
+
+function nop(){}
+
+function onConnectionFailure(reason)
+{
+	if(ConnectionInterrupted==false)
+		bluetoothSerial.disconnect(nop,nop);
+	ConnectionInterrupted=true;
+	window.alert("نشد که وصل بشم");
+}
+
+function onCommunicationFailure(reason)
+{
+	if(ConnectionInterrupted==false)
+		bluetoothSerial.disconnect(nop,nop);
+	ConnectionInterrupted=true;
+	window.alert("ارتباط نا موفق");
+}
+
+
+function onReceiving()
+{
+	bluetoothSerial.readUntil('$', 
+			function(data) {
+				data=data.replace(/ /g,'');
+				data=data.replace(/\s/g,'');
+				if(data=="")
+					return ;
+				var tempDataReceived=data.split(",");
+				for(var i=0; i<tempDataReceived.length; i++)
+				{
+					tempDataReceived=parseInt(tempDataReceived[i]);
+				}
+				MicroData.concat(tempDataReceived);
+				MicroData.splice(MicroDataSize-1,MicroData.length);
+				bluetoothSerial.disconnect(nop,nop);
+			}
+			, onCommunicationFailure);
+}
+
+function onConnect()
+{
+	ConnectionInterrupted=false;
+	bluetoothSerial.write('s', onReceiving, onCommunicationFailure);
+}
+
+function synchData()
+{
+	 /*
+	MicroData[0]=1;
+	// */
+	// /*
+	ConnectionInterrupted=true;
+	bluetoothSerial.connect(BlueDevMacAdr, onConnect, onConnectionFailure);
+	// */
+}
